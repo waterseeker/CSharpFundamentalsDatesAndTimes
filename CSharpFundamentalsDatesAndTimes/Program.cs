4d5bdfeadbf1c9df7e1da552e4a156a6a1f161b0
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 14:08:48 -0500

    added example that formats the datetime in a standard way according to the current culture and one that uses InvariantCulture

--- CSharpFundamentalsDatesAndTimes/Program.cs
+++ CSharpFundamentalsDatesAndTimes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace CSharpFundamentalsDatesAndTimes
 {
@@ -10,10 +11,18 @@ namespace CSharpFundamentalsDatesAndTimes
             Console.WriteLine(myValue.ToString());
 
             //DateTime.ToShortDateString no longer works but 
-            //ToString("d") does the same thing. 
+            //ToString("d") does the same thing
+            //just shows the date
             Console.WriteLine(myValue.ToString("d"));
 
+            //to just show the time
             Console.WriteLine(myValue.ToString("t"));
+
+            //to format the datetime according to the current culture
+            Console.WriteLine(myValue.ToString("D"));
+
+            //same thing but with invariant culture
+            Console.WriteLine(myValue.ToString("D", CultureInfo.InvariantCulture));
 
             Console.ReadLine();
         }
